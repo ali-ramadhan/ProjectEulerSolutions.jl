13687ed67be8318441284c2ad19b8a1f1ddce294
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-03-03 21:26:24 -0700

    Automate including solution modules and test files

--- src/ProjectEulerSolutions.jl
+++ src/ProjectEulerSolutions.jl
@@ -1,8 +1,13 @@
 module ProjectEulerSolutions
 
-for n in 1:25
-    n_padded = lpad(n, 3, '0')
-    include("solutions/problem$n_padded.jl")
+# Find all problem files in the solutions directory
+problem_files = filter(
+    file -> occursin(r"problem\d{3}\.jl$", file),
+    readdir(joinpath(@__DIR__, "solutions"), join=true)
+)
+
+for file in sort(problem_files)
+    include(file)
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,29 +2,16 @@ using Test
 using SafeTestsets
 
 @testset "ProjectEulerSolutions.jl" verbose=true begin
-    @safetestset "Problem 001" include("test_problem001.jl")
-    @safetestset "Problem 002" include("test_problem002.jl")
-    @safetestset "Problem 003" include("test_problem003.jl")
-    @safetestset "Problem 004" include("test_problem004.jl")
-    @safetestset "Problem 005" include("test_problem005.jl")
-    @safetestset "Problem 006" include("test_problem006.jl")
-    @safetestset "Problem 007" include("test_problem007.jl")
-    @safetestset "Problem 008" include("test_problem008.jl")
-    @safetestset "Problem 009" include("test_problem009.jl")
-    @safetestset "Problem 010" include("test_problem010.jl")
-    @safetestset "Problem 011" include("test_problem011.jl")
-    @safetestset "Problem 012" include("test_problem012.jl")
-    @safetestset "Problem 013" include("test_problem013.jl")
-    @safetestset "Problem 014" include("test_problem014.jl")
-    @safetestset "Problem 015" include("test_problem015.jl")
-    @safetestset "Problem 016" include("test_problem016.jl")
-    @safetestset "Problem 017" include("test_problem017.jl")
-    @safetestset "Problem 018" include("test_problem018.jl")
-    @safetestset "Problem 019" include("test_problem019.jl")
-    @safetestset "Problem 020" include("test_problem020.jl")
-    @safetestset "Problem 021" include("test_problem021.jl")
-    @safetestset "Problem 022" include("test_problem022.jl")
-    @safetestset "Problem 023" include("test_problem023.jl")
-    @safetestset "Problem 024" include("test_problem024.jl")
-    @safetestset "Problem 025" include("test_problem025.jl")
+    test_files = filter(
+        file -> occursin(r"^test_problem\d{3}\.jl$", file),
+        readdir(@__DIR__)
+    )
+
+    sort!(test_files)
+
+    for test_file in test_files
+        problem_num = match(r"test_problem(\d{3})\.jl", test_file).captures[1]
+        test_name = "Problem $problem_num"
+        @eval @safetestset $test_name include($test_file)
+    end
 end
