a12ce2e73ed1e08d273809b5b87e00b60a5b10f4
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-03-04 07:38:03 -0700

    Print problem being tested

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,6 +12,7 @@ using SafeTestsets
     for test_file in test_files
         problem_num = match(r"test_problem(\d{3})\.jl", test_file).captures[1]
         test_name = "Problem $problem_num"
+        @info "Testing $test_name..."
         @eval @safetestset $test_name include($test_file)
     end
 end
