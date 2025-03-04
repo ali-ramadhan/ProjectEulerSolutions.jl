b05fe9a8a8f2c3d974290e265288ac2150d5e9ed
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-03-03 23:25:40 -0700

    Solve problem 29

---
+++ test/test_problem029.jl
@@ -0,0 +1,6 @@
+using ProjectEulerSolutions.Problem029: count_distinct_powers, solve
+
+# Test with the example from the problem statement
+@test count_distinct_powers(2, 5, 2, 5) == 15
+
+@test solve() == 9183
