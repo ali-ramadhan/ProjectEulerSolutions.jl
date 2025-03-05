2033e9294080a0c7c5aba9f2e579767ebea7fd38
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-03-04 22:32:15 -0700

    Solve problem 49

---
+++ test/test_problem049.jl
@@ -0,0 +1,12 @@
+using ProjectEulerSolutions.Problem049: sieve_of_eratosthenes, get_sorted_digits, find_prime_permutation_sequence, solve
+
+@test get_sorted_digits(1487) == get_sorted_digits(4817)
+@test get_sorted_digits(4817) == get_sorted_digits(8147)
+@test get_sorted_digits(1234) != get_sorted_digits(2345)
+
+known_sequence = (1487, 4817, 8147)
+@test get_sorted_digits(known_sequence[1]) == get_sorted_digits(known_sequence[2])
+@test get_sorted_digits(known_sequence[2]) == get_sorted_digits(known_sequence[3])
+@test known_sequence[2] - known_sequence[1] == known_sequence[3] - known_sequence[2]
+
+@test solve() == 296962999629
