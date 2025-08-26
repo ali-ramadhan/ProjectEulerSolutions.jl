d3a0317d36c271daec999d4a65a725d2673b5543
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-08-25 22:45:26 -0600

    Improve problem 10 docstring

--- test/test_problem010.jl
+++ test/test_problem010.jl
@@ -1,5 +1,8 @@
+using Test
 using ProjectEulerSolutions.Problem010: sum_of_primes_below, solve
 
+# Test helper function with the example from the problem description
 @test sum_of_primes_below(10) == 17
 
+# Correct answer
 @test solve() == 142913828922
