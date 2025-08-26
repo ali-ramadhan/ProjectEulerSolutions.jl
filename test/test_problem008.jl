6e66d102d94b550ec8515e3a958bdeed4b65ab59
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-08-25 22:35:45 -0600

    Improve problem 8 docstring

--- test/test_problem008.jl
+++ test/test_problem008.jl
@@ -1,7 +1,11 @@
+using Test
 using ProjectEulerSolutions.Problem008: product_of_digits, largest_product_in_series, solve
 
+# Test helper functions with examples from the problem description
 @test product_of_digits("9989") == 5832
 
+# Test the function with the 4-digit example from the problem statement
 @test largest_product_in_series(4)[1] == 5832
 
+# Correct answer
 @test solve() == 23514624000
