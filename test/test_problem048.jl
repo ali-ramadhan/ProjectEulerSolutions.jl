edf14bf7e937dad99db7faa950cc8f799b39b4aa
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-08-27 07:04:45 -0600

    Fix problem 48 tests

--- test/test_problem048.jl
+++ test/test_problem048.jl
@@ -1,5 +1,5 @@
 using ProjectEulerSolutions.Problem048: last_ten_digits_of_self_powers, solve
 
-@test last_ten_digits_of_self_powers(10) == 10405071317 % 10^10
+@test last_ten_digits_of_self_powers(10, 10^20) == 10405071317
 
 @test solve() == 9110846700
