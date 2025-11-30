0e60d76ef00b2c25213ecf4a15e4b7c52f176345
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-11-30 01:36:43 -0700

    Rerun benchmarks for problem 9

--- blog/problem0009.jl
+++ blog/problem0009.jl
@@ -1,7 +1,7 @@
 using BenchmarkTools
 using ProjectEulerSolutions.Utils: save_benchmark
-using ProjectEulerSolutions.Problem0009: find_pythagorean_triplet
+using ProjectEulerSolutions.Problem0009: find_pythagorean_triplets
 
-@show find_pythagorean_triplet()
-benchmark_1000 = @benchmark find_pythagorean_triplet()
+@show find_pythagorean_triplets(1000)
+benchmark_1000 = @benchmark find_pythagorean_triplets(1000)
 save_benchmark(benchmark_1000, 9, "n_1000")
