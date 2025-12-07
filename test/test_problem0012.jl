869cfd0769c03d3d0c64b1a6d55898eb3644ea51
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-12-07 07:23:34 -0700

    Update tests for problem 12

--- test/test_problem0012.jl
+++ test/test_problem0012.jl
@@ -1,6 +1,6 @@
 using Test
 using ProjectEulerSolutions.Problem0012: find_first_triangle_with_divisors, solve
 
-@test find_first_triangle_with_divisors(5) == 28
+@test find_first_triangle_with_divisors(5) == (7, 28)
 
 @test solve() == 76576500
