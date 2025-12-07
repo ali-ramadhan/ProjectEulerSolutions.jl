a7dc01b14fb5bcd1827118007ad0f9529b285450
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-12-07 14:58:25 -0700

    Benchmark problem 15

---
+++ blog/problem0015.jl
@@ -0,0 +1,7 @@
+using BenchmarkTools
+using ProjectEulerSolutions.Utils: save_benchmark
+using ProjectEulerSolutions.Problem0015: count_lattice_paths
+
+@show count_lattice_paths(20, 20)
+b_20x20 = @benchmark count_lattice_paths(20, 20)
+save_benchmark(b_20x20, 15, "count_lattice_paths_20x20")
