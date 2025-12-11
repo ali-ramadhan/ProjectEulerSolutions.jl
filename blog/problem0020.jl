0b93fce5ef01e01b288590e5ef7a1100f3b9ee23
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-12-10 18:00:51 -0700

    Benchmark problem 20

---
+++ blog/problem0020.jl
@@ -0,0 +1,11 @@
+using BenchmarkTools
+using ProjectEulerSolutions.Utils: save_benchmark
+using ProjectEulerSolutions.Problem0020: sum_of_factorial_digits
+
+@show sum_of_factorial_digits(100)
+benchmark1 = @benchmark sum_of_factorial_digits(100)
+save_benchmark(benchmark1, "problem-0020", "factorial_100")
+
+@show sum_of_factorial_digits(1000000)
+benchmark2 = @benchmark sum_of_factorial_digits(1000000) samples=100 evals=1 seconds=1000
+save_benchmark(benchmark2, "problem-0020", "factorial_1M")
