d13db7aa5e86c7d84efa409a03ecf407a220a704
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-12-13 09:05:57 -0700

    Redo and benchmark bonus problem 18i

---
+++ blog/bonus_18i.jl
@@ -0,0 +1,6 @@
+using BenchmarkTools
+using ProjectEulerSolutions.Utils: save_benchmark
+using ProjectEulerSolutions.Bonus18i: sum_R_mod_p
+
+b = @benchmark sum_R_mod_p(1_000_000_000, 1_100_000_000) samples=20 evals=1 seconds=1000
+save_benchmark(b, "bonus-18i", "solution")
