e2ac034d1e3b5166f2a4eb1c594faa542bc52113
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-12-15 23:28:48 -0700

    Properly disable and enable logging when benchmarking Heegner

--- benchmarks/bonus_heegner.jl
+++ benchmarks/bonus_heegner.jl
@@ -3,12 +3,14 @@ using BenchmarkTools
 using ProjectEulerSolutions.Utils.Benchmarks
 using ProjectEulerSolutions.BonusHeegner: find_closest_cos_to_integer
 
-Logging.disable_logging(Logging.Info)
-
 @show find_closest_cos_to_integer(1000)
+Logging.disable_logging(Logging.Info)
 b = @benchmark find_closest_cos_to_integer(1000)
+Logging.disable_logging(Logging.BelowMinLevel)
 save_benchmark(b, "bonus-heegner", "n_1k")
 
 @show find_closest_cos_to_integer(1000000)
+Logging.disable_logging(Logging.Info)
 b = @benchmark find_closest_cos_to_integer(1000000) samples=5 evals=1 seconds=1000
+Logging.disable_logging(Logging.BelowMinLevel)
 save_benchmark(b, "bonus-heegner", "n_1M")
