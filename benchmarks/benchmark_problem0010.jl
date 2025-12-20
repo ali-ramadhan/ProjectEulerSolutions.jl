ac11893d30f94161b4362a06bce6757614c8e741
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-12-20 12:04:08 -0700

    Update problem 10 benchmarks (test 200M primes)

--- benchmarks/benchmark_problem0010.jl
+++ benchmarks/benchmark_problem0010.jl
@@ -6,6 +6,6 @@ using ProjectEulerSolutions.Problem0010
 benchmark_2M = @benchmark sum_of_primes_below(2_000_000)
 save_benchmark(benchmark_2M, "problem-0010", "sum_of_primes_below_2M")
 
-@show sum_of_primes_below(100_000_000)[2]
-benchmark_100M = @benchmark sum_of_primes_below(100_000_000) samples=100 evals=1 seconds=1000
-save_benchmark(benchmark_100M, "problem-0010", "sum_of_primes_below_100M")
+@show sum_of_primes_below(200_000_000)[2]
+benchmark_200M = @benchmark sum_of_primes_below(200_000_000) samples=100 evals=1 seconds=1000
+save_benchmark(benchmark_200M, "problem-0010", "sum_of_primes_below_200M")
