46eaba72fbba2cff656718cf41fa8bdc9cec4b6b
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-12-20 23:32:52 -0700

    Forgot to include updates to problem 21 benchmarks

--- benchmarks/benchmark_problem0021.jl
+++ benchmarks/benchmark_problem0021.jl
@@ -3,9 +3,17 @@ using ProjectEulerSolutions.Utils.Benchmarks
 using ProjectEulerSolutions.Problem0021
 
 @show sum_of_amicable_numbers(10000)
-benchmark1 = @benchmark sum_of_amicable_numbers(10000)
-save_benchmark(benchmark1, "problem-0021", "limit_10k")
+benchmark_10k = @benchmark sum_of_amicable_numbers(10000)
+save_benchmark(benchmark_10k, "problem-0021", "limit_10k")
+
+@show sum_of_amicable_numbers(100000)
+benchmark_100k = @benchmark sum_of_amicable_numbers(100000)
+save_benchmark(benchmark_100k, "problem-0021", "limit_100k")
 
 @show sum_of_amicable_numbers(1000000)
-benchmark2 = @benchmark sum_of_amicable_numbers(1000000) samples=100 evals=1 seconds=1000
-save_benchmark(benchmark2, "problem-0021", "limit_1M")
+benchmark_1M = @benchmark sum_of_amicable_numbers(1000000) samples=100 evals=1 seconds=1000
+save_benchmark(benchmark_1M, "problem-0021", "limit_1M")
+
+@show sum_of_amicable_numbers(10000000)
+benchmark_10M = @benchmark sum_of_amicable_numbers(10000000) samples=100 evals=1 seconds=1000
+save_benchmark(benchmark_10M, "problem-0021", "limit_10M")
