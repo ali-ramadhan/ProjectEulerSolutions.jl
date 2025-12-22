b85bc288fc7823095f20fe0ecfb8ca418a1402fe
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-12-22 07:18:02 -0700

    Fix tests

--- test/problems/test_problem0029.jl
+++ test/problems/test_problem0029.jl
@@ -4,7 +4,7 @@ using ProjectEulerSolutions.Problem0029
 
 # Test with the example from the problem statement
 # 2 ≤ a ≤ 5, 2 ≤ b ≤ 5 gives 15 distinct terms
-@test count_distinct_powers(2, 5, 2, 5) == 15
+@test count_distinct_powers(5) == 15
 
 # Correct answer
 @test_answer solve() "0029"
