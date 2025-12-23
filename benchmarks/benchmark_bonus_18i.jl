8e32b81184c0422d355f01631142923acc8fd22b
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-12-22 22:19:12 -0700

    Cleaner

--- benchmarks/benchmark_bonus_18i.jl
+++ benchmarks/benchmark_bonus_18i.jl
@@ -1,6 +1,6 @@
 using BenchmarkTools
 using ProjectEulerSolutions.Utils.Benchmarks
-using ProjectEulerSolutions.Bonus18i: sum_R_mod_p
+using ProjectEulerSolutions.Bonus18i
 
 b = @benchmark sum_R_mod_p(1_000_000_000, 1_100_000_000) samples=100 evals=1 seconds=600
 save_benchmark(b, "bonus-18i", "solution"; thread_count=Threads.nthreads())
