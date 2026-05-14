67f0c6f33ad1bb1b113e09cf9baee51ed73ebacf
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2026-05-14 10:55:07 -0600

    Bonus `contfrac`: Benchmark Q(13) and Q(14)

--- benchmarks/benchmark_bonus_contfrac.jl
+++ benchmarks/benchmark_bonus_contfrac.jl
@@ -13,3 +13,11 @@ save_benchmark(b, "bonus-contfrac", "Q_10")
 @show compute_Q(12)
 b = @benchmark compute_Q(12) samples=100 evals=1 seconds=600
 save_benchmark(b, "bonus-contfrac", "Q_12")
+
+@show compute_Q(13)
+b = @benchmark compute_Q(13) samples=20 evals=1 seconds=600
+save_benchmark(b, "bonus-contfrac", "Q_13")
+
+@show compute_Q(14)
+b = @benchmark compute_Q(14) samples=5 evals=1 seconds=600
+save_benchmark(b, "bonus-contfrac", "Q_14")
