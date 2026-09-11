c06b2de3984f39085a513a49c86b0be5a569c455
ali-ramadhan <ali.ramadhan@atdepthmrv.com>
2026-09-11 03:55:29 +0000

    Handle small limits in even Fibonacci sums

--- test/problems/test_problem0002.jl
+++ test/problems/test_problem0002.jl
@@ -2,5 +2,14 @@ using Test
 using ProjectEulerSolutions.Utils.AnswerHashing
 using ProjectEulerSolutions.Problem0002
 
+@test sum_even_fibonacci(0) == 0
+@test sum_even_fibonacci(1) == 0
+for limit in 2:7
+    @test sum_even_fibonacci(limit) == 2
+end
+@test sum_even_fibonacci(8) == 10
+@test sum_even_fibonacci(9) == 10
+@test sum_even_fibonacci(34) == 44
+
 # Correct answer
 @test_answer solve() "0002"
