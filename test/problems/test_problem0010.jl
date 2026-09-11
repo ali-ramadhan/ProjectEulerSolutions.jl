98b8454dbc1a43205524036e5658354e534698a5
ali-ramadhan <ali.ramadhan@atdepthmrv.com>
2026-09-11 03:55:29 +0000

    Exclude the limit from sums of primes below it

--- test/problems/test_problem0010.jl
+++ test/problems/test_problem0010.jl
@@ -5,5 +5,12 @@ using ProjectEulerSolutions.Problem0010
 # Test helper function with the example from the problem description
 @test sum_of_primes_below(10) == 17
 
+@test sum_of_primes_below(0) == 0
+@test sum_of_primes_below(1) == 0
+@test sum_of_primes_below(2) == 0
+@test sum_of_primes_below(3) == 2
+@test sum_of_primes_below(5) == 5
+@test sum_of_primes_below(6) == 10
+
 # Correct answer
 @test_answer solve() "0010"
